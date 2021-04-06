2fc046ca03111d622807aea52e281d8c7c7851a3
Ioan <ioan.sava28@gmail.com>
2021-04-06 20:27:03 +0300

    feat: Integrate user role

--- FE-Hot-Diggety-Dog/Models/User.cs
+++ FE-Hot-Diggety-Dog/Models/User.cs
@@ -6,6 +6,7 @@ namespace FE_Hot_Diggety_Dog.Models
     {
         public Guid Id { get; set; }
         public string Username { get; set; }
+        public string Role { get; set; }
         public string Token { get; set; }
     }
 }
