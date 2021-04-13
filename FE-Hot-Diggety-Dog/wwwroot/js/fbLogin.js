a6b02ec2dc4cbd1e2fabb8e426ee72b9ab2b7e1c
vTrStefaN <stef.lngf99@gmail.com>
2021-04-13 17:49:56 +0300

    Facebook Login

---
+++ FE-Hot-Diggety-Dog/wwwroot/js/fbLogin.js
@@ -0,0 +1,12 @@
+﻿window.fbLogin = function () {
+    window.FB.login(function (response) {
+        if (response.status === 'connected') {
+            console.log(response)            
+        } else {
+            console.log("Not logged");
+        }
+
+        DotNet.invokeMethodAsync('FE-Hot-Diggety-Dog', 'FbLoginProcessCallback', response);
+
+    }, { scope: 'public_profile, email' });
+}
