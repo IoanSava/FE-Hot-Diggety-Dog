c8e2a6c37e4728ad2058905648ac75af918143e2
Bogdan <radumugur1997@gmail.con>
2021-05-17 22:08:55 +0300

    fe update

--- FE-Hot-Diggety-Dog/Models/AddProductToStandRequest.cs
+++ FE-Hot-Diggety-Dog/Models/AddProductToStandRequest.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+
 namespace FE_Hot_Diggety_Dog.Models
 {
     public class AddProductToStandRequest
-{
+    {
         [Required]
         public Guid ProductId { get; set; }
+
         [Required]
         public int Quantity { get; set; }
     }
--- FE-Hot-Diggety-Dog/Models/UpdateStandProductsRequest.cs
+++ FE-Hot-Diggety-Dog/Models/UpdateStandProductsRequest.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 namespace FE_Hot_Diggety_Dog.Models
 {
     public class UpdateStandProductsRequest
-{
-    public Guid StandId { set; get; }
+    {
+        public Guid StandId { set; get; }
         public List<AddProductToStandRequest> Products { set; get; } = new List<AddProductToStandRequest>();
-}
+    }
 }
