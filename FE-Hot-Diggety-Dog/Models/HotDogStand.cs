ebab584acaa971996786ee11b80ebf0f236360e1
Bogdan <radumugur1997@gmail.con>
2021-05-17 20:04:38 +0300

    Stands Management Frond End Update

--- FE-Hot-Diggety-Dog/Models/HotDogStand.cs
+++ FE-Hot-Diggety-Dog/Models/HotDogStand.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace FE_Hot_Diggety_Dog.Models
 {
@@ -7,5 +8,6 @@ namespace FE_Hot_Diggety_Dog.Models
         public Guid Id { get; set; }
         public Guid OperatorId { get; set; }
         public string Address { get; set; }
+        public virtual ICollection<HotDogStandProduct> StandProducts { get; set; }
     }
 }
---
+++ FE-Hot-Diggety-Dog/Models/HotDogStandProduct.cs
@@ -0,0 +1,12 @@
+﻿using FE_Hot_Diggety_Dog.Models;
+using System;
+
+namespace FE_Hot_Diggety_Dog.Models
+{
+
+    public class HotDogStandProduct
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
